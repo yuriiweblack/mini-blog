82dcd38356395027609d06108dbb3f7cbc5104dc
yuriiweblack <yuriiwebblack@gmail.com>
2023-04-02 19:41:01 +0300

    base.html-60%

---
+++ main.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///project.db'
+db = SQLAlchemy(app)
+
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    title = db.Column(db.String(100), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"Article: {self.id}, {self.username}, {self.title}"
+
+
+with app.app_context():
+    db.create_all()
+
+
+@app.route('/')
+@app.route('/home')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+
+@app.route('/user/<string:name>/<int:id>')
+def user(name, id):
+    return f"User page {name} - id {id}"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
